b79bb8068063f72d1bb4ae49666b00aab1ed9f32
MM22012011 <85019183+MM22012011@users.noreply.github.com>
2022-08-28 12:25:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 Metalica = "";
 HW = "";
 
+rightWristX = 0;
+rightWristY = 0;
+
+leftWristX = 0;
+leftWristY = 0;
+
+scoreleftWrist = 0;
+
 function preload()
 {
     Metalica = loadSound("MasterOfPuppets.mp3");
@@ -12,12 +20,63 @@ function setup()
 {
      canvas = createCanvas(600, 500);
      canvas.center();
+
      video = createCapture(VIDEO);
      video.hide();
+
+     poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
 }
 
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+  }
+
+  function gotPoses(results)
+  {
+    if(results.length > 0)
+    {
+      console.log(results);
+  
+      scoreleftWrist =  results[0].pose.keypoints[9].score;
+    
+
+      rightWristX = results[0].pose.rightWrist.x;
+      rightWristY = results[0].pose.rightWrist.y;
+      console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
+  
+      leftWristX = results[0].pose.leftWrist.x;
+      leftWristY = results[0].pose.leftWrist.y;
+      console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
+          
+    }
+  }
+  
 
 function draw()
 {
     image(video, 0, 0, 600, 500); 
+    
+	fill("#FF0000");
+	stroke("#FF0000");
+
+    HW.isPlaying()
+
+    if (scoreleftWrist > 0.2)
+	{
+		circle(leftWristX, leftWristY, 20);
+
+        Metalica.stop()
+
+	}
+
+    if (Metalica == Metalica.stop())
+    {
+        function play()
+        {
+            HW.play()
+        }
+    }
 }
+
+
